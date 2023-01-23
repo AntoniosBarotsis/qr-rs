4f32a10bb57ef470ca58d50f9e5970634669e718
Antonios Barotsis <a.barotsis@student.tudelft.nl>
2023-01-23 23:16:29 +0100

    Fixed header name

--- common/src/lib.rs
+++ common/src/lib.rs
@@ -16,13 +16,15 @@ pub fn hex_to_rgb(hex: &str) -> Option<Rgb> {
 }
 
 /// Fetches an image from the passed link and returns its bytes.
+/// 
+/// The method additionally verifies that the `Content-Type` is of type `image`.
 pub fn read_image_bytes(link: &str) -> Option<Vec<u8>> {
   let resp = reqwest::blocking::get(link).ok()?;
 
   // TODO Test with actual links
   let content_type_is_image = resp
     .headers()
-    .get("content")
+    .get("Content-Type")
     .map(|x| x.to_str())?
     .ok()
     .map(|content_type| content_type.contains("image"))?;
