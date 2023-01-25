6422da3c22eed8abfa89dc0d2845734cd6ca814a
unknown <a.barotsis@student.tudelft.nl>
2023-01-25 15:08:13 +0100

    Remove duplicate (sync) method

--- common/src/lib.rs
+++ common/src/lib.rs
@@ -18,28 +18,6 @@ pub fn hex_to_rgb(hex: &str) -> Option<Rgb> {
 /// Fetches an image from the passed link and returns its bytes.
 ///
 /// The method additionally verifies that the `Content-Type` is of type `image`.
-pub fn read_image_bytes(link: &str) -> Option<Vec<u8>> {
-  let resp = reqwest::blocking::get(link).ok()?;
-
-  // TODO Test with actual links
-  let content_type_is_image = resp
-    .headers()
-    .get("Content-Type")
-    .map(reqwest::header::HeaderValue::to_str)?
-    .ok()
-    .map(|content_type| content_type.contains("image"))?;
-
-  if !content_type_is_image {
-    return None;
-  }
-
-  let resp_bytes = resp.bytes().ok()?;
-  let res = resp_bytes.into_iter().collect::<Vec<_>>();
-
-  Some(res)
-}
-
-/// An exact copy of [`read_image_bytes`] but async.
 pub async fn read_image_bytes_async(link: &str) -> Option<Vec<u8>> {
   let resp = reqwest::get(link).await.ok()?;
 
