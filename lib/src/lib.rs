9a2a9af41645c955608f9a496703aa20d04e25be
Antonios Barotsis <a.barotsis@student.tudelft.nl>
2023-03-09 18:04:04 +0100

    Move tests to src

--- lib/src/lib.rs
+++ lib/src/lib.rs
@@ -257,3 +257,53 @@ fn distance(c: u32, x: u32, y: u32) -> f64 {
   #[allow(clippy::cast_possible_wrap)]
   f64::from((c as i32 - x as i32).pow(2) + (c as i32 - y as i32).pow(2)).sqrt()
 }
+
+#[cfg(test)]
+mod tests {
+  use crate::{error::Error, QrCodeBuilder, Rgb};
+
+  const LOGO: &[u8] = include_bytes!("../../assets/logo.png");
+
+  #[test]
+  fn empty_link() {
+    let res = QrCodeBuilder::new("", LOGO)
+      .build()
+      .expect_err("Empty link should fail.");
+
+    assert!(matches!(res, Error::InputError(_)));
+  }
+
+  #[test]
+  fn size_too_small() {
+    let res = QrCodeBuilder::new("link", LOGO)
+      .with_size(199)
+      .build()
+      .expect_err("Small size should fail.");
+
+    assert!(matches!(res, Error::InputError(_)));
+  }
+
+  #[test]
+  fn size_too_big() {
+    let res = QrCodeBuilder::new("link", LOGO)
+      .with_size(1001)
+      .build()
+      .expect_err("Big size should fail.");
+
+    assert!(matches!(res, Error::InputError(_)));
+  }
+
+  #[test]
+  fn valid() {
+    let link = "https://github.com/AntoniosBarotsis/qr-rs";
+    let size = 600;
+    let bg_color = Rgb([255, 0, 0]);
+
+    let res = QrCodeBuilder::new(link, LOGO)
+      .with_size(size)
+      .with_bg_color(bg_color)
+      .build();
+
+    assert!(matches!(res, Ok(_)));
+  }
+}
